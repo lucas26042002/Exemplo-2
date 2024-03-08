10484da47e12053a36c24badf94b2e315b3ea9ef
Lucas Schneider Ludwig <114614576+lucas26042002@users.noreply.github.com>
2024-03-08 18:50:55 -0300

    Create PesosDeAtributos.java

---
+++ PesosDeAtributos.java
@@ -0,0 +1,17 @@
+package Dungeon_Clash;
+
+public class PesosDeAtributos {
+  private double forca;
+
+  public PesosDeAtributos(double forca) {
+    this.forca = forca;
+  }
+
+  public double getForca() {
+    return forca;
+  }
+
+  public void setForca(double forca) {
+    this.forca = forca;
+  }
+}
